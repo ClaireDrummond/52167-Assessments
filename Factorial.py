321e28c83aa0bd5ea236a588e358d042b2b3718c
Claire Drummond <g00364781@gmit.ie>
2018-03-20 20:48:10 +0000

    Fixing Function Definition in Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -2,7 +2,7 @@
 # Factorial Numbers Exercise 6
 
 def factorial(n): #return the factorial of n
-    num = 1
+    num = 1 
     while n >= 1:
         num = num * n
         n = n - 1
@@ -13,7 +13,7 @@ print("The factorial of number 10 is:",factorial(10))
 
 # Alternative Solution
 # ref: https://stackoverflow.com/questions/5136447/function-for-factorial-in-python
-from math import factorial
+from math import factorial # Function is in Math Module
 print("The factorial of number 5 is:",factorial(5))
 print("The factorial of number 7 is:",factorial(7))
 print("The factorial of number 10 is:",factorial(10))
@@ -21,14 +21,14 @@ print("The factorial of number 10 is:",factorial(10))
 # Alternative Solution Option from Topic 7 Notes on Functions
 # ref: https://nbviewer.jupyter.org/github/ianmcloughlin/python-fundamentals-notes/blob/master/functions-modules.ipynb#
 
-def factorial(n):
-    """Return the factorial of n."""
+def factorialb(m):
+    """Return the factorialb of m."""
     ans = 1
-    for i in range(2, n + 1):
+    for i in range(2, m + 1):
         ans = ans * i
     return ans
 
 
-print(factorial(5))
-print(factorial(7))
-print(factorial(10)) 
+print(factorialb(5))
+print(factorialb(7))
+print(factorialb(10)) 
